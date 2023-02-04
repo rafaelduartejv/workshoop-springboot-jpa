852f9f5d90a942d2899ca53b8df1f1e1299e3fbb
rafaelduartejv <rafaelduarte.jv@gmail.com>
2023-02-04 13:50:20 -0300

    Order, Instant, ISO 8601 pt-1

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -23,7 +23,9 @@ public class TestConfig implements CommandLineRunner {
     public void run(String... args) throws Exception {
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+        User u3 = new User(null, "Rafael", "rafael@gmail.com", "548451144", "123456");
+        User u4 = new User(null, "Ione", "Ione@gmail.com", "212215884", "123456");
 
-        userRepository.saveAll(Arrays.asList(u1,u2));
+        userRepository.saveAll(Arrays.asList(u1,u2,u3,u4));
     }
 }
